a6f8c3aa48bc1f05237caa3bc6a6ae4dd7e53c1e
Jktym <yorkdan@bk.ru>
2026-07-19 20:09:58 +0700

    add unity tests

--- module2/2.2/main.c
+++ module2/2.2/main.c
@@ -1,45 +1,5 @@
-#include <stdio.h>
-#include "calc.h"
+#include "menu.h"
 
-void menu() {
-    const char* main_menu [] = {"-------ГЛАВНОЕ МЕНЮ-------\nВыберите действие:\n1.Сложение\n2.Вычитание\n3.Умножение\n4.Деление\n0.Выход\n"};
-    while(1) {
-        printf("%s", *main_menu);
-        int choice = -1;
-        scanf("%d", &choice);
-        float x, y;
-        if(choice && choice >= 1 && choice <= 4) {
-            printf("Введите первый аргумент: ");
-            scanf(" %f", &x);
-            printf("Введите второй аргумент: ");
-            scanf(" %f", &y);
-        }
-        switch (choice) {
-            case 1:
-                printf("%.3f + %.3f = %.3f\n", x, y, add(x, y));
-                break;
-            case 2:
-                printf("%.3f - %.3f = %.3f\n", x, y, sub(x, y));
-                break;
-            case 3:
-                printf("%.3f * %.3f = %.3f\n", x, y, mul(x, y));
-                break;
-            case 4:
-                if (y)
-                printf("%.3f / %.3f = %f\n", x, y, div(x, y));
-                else printf("На 0 делить нельзя!!\n");
-                break;
-            case 0:
-                return;
-            default:
-                printf("Нет такого пункта! Попробуйте ещё раз\n");
-                int c;
-                while((c = getchar()) != '\n' && c != EOF);
-                break;
-        }
-    }
-
-}
 
 int main() {
     menu();
---
+++ module2/2.2/test.c
@@ -0,0 +1,153 @@
+#include <string.h>
+#include "calc.h"
+#include "menu.h"
+#include "unity.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void simple_test_add(void) {
+    float result = add(100, 200);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 300, result);
+}
+
+void simple_test_sub(void) {
+    float result = sub(150, 50);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 100, result);
+}
+
+void simple_test_mul(void) {
+    float result = mul(10, 15);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 150, result);
+}
+
+void simple_test_div(void) {
+    float result = div(50, 5);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 10, result);
+}
+
+void negative_test_add(void) {
+    float result = add(-100, 50);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, -50, result);
+}
+
+void negative_test_add_2(void) {
+    float result = add(-100, -50);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, -150, result);
+}
+
+void negative_test_sub(void) {
+    float result = sub(-100, 50);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, -150, result);
+}
+
+void negative_test_sub_2(void) {
+    float result = sub(-100, -50);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, -50, result);
+}
+
+void negative_test_mul(void) {
+    float result = mul(-100, 5);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, -500, result);
+}
+
+void negative_test_mul_2(void) {
+    float result = mul(-100, -5);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 500, result);
+}
+
+void negative_test_div(void) {
+    float result = div(-500, 5);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, -100, result);
+}
+
+void negative_test_div_2(void) {
+    float result = div(-500, -5);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 100, result);
+}
+
+void test_zero_mul(void) {
+    float result = mul(1000, 0);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 0, result);
+}
+
+void test_zero_div(void) {
+    FILE *fake_stdin = tmpfile();
+    fputs("4\n100\n0\n0\n", fake_stdin);
+    rewind(fake_stdin);
+
+    FILE *fake_stdout = tmpfile();
+    
+    FILE *old_stdin = stdin;
+    FILE *old_stdout = stdout;
+    stdin = fake_stdin;
+    stdout = fake_stdout;       
+    menu();
+
+    fflush(fake_stdout);
+    stdin = old_stdin;
+    stdout = old_stdout;
+    
+    rewind(fake_stdout);
+    char output_buffer[4096] = {0};
+    size_t bytes_read = fread(output_buffer, 1, sizeof(output_buffer) - 1, fake_stdout);
+    output_buffer[bytes_read] = '\0';
+
+    fclose(fake_stdin);
+    fclose(fake_stdout);
+
+    TEST_ASSERT_TRUE_MESSAGE(
+        strstr(output_buffer, "На 0 делить нельзя!!") != NULL,
+        "Ожидалось сообщение об ошибке.\n"
+    );
+}
+
+void test_unexpected_sym() {
+    FILE *fake_stdin = tmpfile();
+    fputs("dsad\n0\n", fake_stdin);
+    rewind(fake_stdin);
+
+    FILE *fake_stdout = tmpfile();
+
+    FILE *old_stdin = stdin;
+    FILE *old_stdout = stdout;
+    stdin = fake_stdin;
+    stdout = fake_stdout;
+    menu();
+    fflush(fake_stdout);
+    stdin = old_stdin;
+    stdout = old_stdout;
+
+    rewind(fake_stdout);
+    char output_buffer [4096] = {0};
+    size_t bytes_read = fread(output_buffer, 1, sizeof(output_buffer) - 1, fake_stdout);
+    output_buffer[bytes_read] = '\0';
+
+    fclose(fake_stdin);
+    fclose(fake_stdout);
+
+    TEST_ASSERT_TRUE_MESSAGE(
+        strstr(output_buffer, "Нет такого пункта! Попробуйте ещё раз") != NULL, 
+        "Ожидало сообщение об ошибке.\n"
+    );
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(simple_test_add);
+    RUN_TEST(simple_test_sub);
+    RUN_TEST(simple_test_mul);
+    RUN_TEST(simple_test_div);
+    RUN_TEST(negative_test_add);
+    RUN_TEST(negative_test_add_2);
+    RUN_TEST(negative_test_sub);
+    RUN_TEST(negative_test_sub_2);
+    RUN_TEST(negative_test_mul);
+    RUN_TEST(negative_test_mul_2);
+    RUN_TEST(negative_test_div);
+    RUN_TEST(negative_test_div_2);
+    RUN_TEST(test_zero_mul);
+    RUN_TEST(test_zero_div);
+    RUN_TEST(test_unexpected_sym);
+    return UNITY_END();
+}

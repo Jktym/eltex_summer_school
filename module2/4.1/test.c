edff50448f1149224825b0544c0adef53c4d3ad3
Jktym <yorkdan@bk.ru>
2026-08-19 13:08:54 +0700

    delete test file

--- module2/4.1/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char buffer [10];
-    char buffptr = NULL
-    printf("Отчество:   %s\n", c->second_name[0] ? c->second_name : "(не указано)");
-    return 0;
-}

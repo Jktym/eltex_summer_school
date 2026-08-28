30c9b9ccba5446e76cb7e48578c8954b74f0c392
Jktym <yorkdan@bk.ru>
2026-08-28 09:57:55 +0700

    fixed

--- module5/lib3/blink_leds.c
+++ module5/lib3/blink_leds.c
@@ -1,111 +1,126 @@
 /*
- * kbleds.c − Blink keyboard leds until the module is unloaded.(modified for > 4.15)
+ * Blink keyboard LEDs until the module is unloaded.
  */
-#include <linux/module.h>
-#include <linux/configfs.h>
+#include <linux/console.h>
 #include <linux/init.h>
-#include <linux/tty.h>          /* For fg_console, MAX_NR_CONSOLES */
-#include <linux/kd.h>           /* For KDSETLED */
-#include <linux/vt.h>
-#include <linux/console_struct.h>       /* For vc_cons */
-#include <linux/vt_kern.h>
+#include <linux/kd.h>
+#include <linux/kobject.h>
+#include <linux/module.h>
+#include <linux/sysfs.h>
 #include <linux/timer.h>
+#include <linux/tty.h>
+#include <linux/vt_kern.h>
 
-#define BLINK_DELAY   HZ/5
-#define ALL_LEDS_ON   0x07
-#define RESTORE_LEDS  0xFF
+#define BLINK_DELAY    (HZ / 10)
+#define ALL_LEDS_ON    0x07
+#define RESTORE_LEDS   0xFF
 
-MODULE_DESCRIPTION("Example module illustrating the use of Keyboard LEDs.");
+MODULE_DESCRIPTION("Example module illustrating the use of keyboard LEDs");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Yerkenov_Daniyar");
-static struct timer_list my_timer;
-static struct tty_driver *my_driver;
-static int _kbledstatus = 0;
-static char *dirname = "systest";
 
+static struct timer_list blink_timer;
+static struct tty_struct *console_tty;
+static struct kobject *leds_kobject;
+static unsigned int led_mask;
+static unsigned int led_status = RESTORE_LEDS;
 
-static struct kobject *example_kobject;
-static int test;
-
-static ssize_t foo_show(struct kobject *kobj, struct kobj_attribute *attr,
-                      char *buf)
+static int set_keyboard_leds(unsigned int mask)
 {
-        return sprintf(buf, "%d\n", test);
+	return console_tty->driver->ops->ioctl(console_tty, KDSETLED, mask);
 }
 
-static void my_timer_func(struct timer_list *ptr)
+static void blink_timer_func(struct timer_list *timer)
 {
-        int *pstatus = &_kbledstatus;
-        if (*pstatus == test)
-                *pstatus = RESTORE_LEDS;
-        else
-                *pstatus = test;
-        (my_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED,
-                            *pstatus);
-        my_timer.expires = jiffies + BLINK_DELAY;
-        add_timer(&my_timer);
+	unsigned int mask = READ_ONCE(led_mask);
+
+	led_status = led_status == mask ? RESTORE_LEDS : mask;
+	set_keyboard_leds(led_status);
+	mod_timer(timer, jiffies + BLINK_DELAY);
 }
 
-static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr,
-                      const char *buf, size_t count)
+static ssize_t led_mask_show(struct kobject *kobj,
+			     struct kobj_attribute *attr, char *buf)
 {
-        sscanf(buf, "%du", &test);
-        timer_setup(&my_timer, my_timer_func, 0);
-        my_timer.expires = jiffies + BLINK_DELAY;
-        add_timer(&my_timer);
-        return count;
+	return sysfs_emit(buf, "%u\n", READ_ONCE(led_mask));
 }
 
+static ssize_t led_mask_store(struct kobject *kobj,
+			      struct kobj_attribute *attr,
+			      const char *buf, size_t count)
+{
+	unsigned int mask;
+	int error;
 
-static struct kobj_attribute foo_attribute =__ATTR(test, 0660, foo_show,
-                                                   foo_store);
+	error = kstrtouint(buf, 0, &mask);
+	if (error)
+		return error;
+	if (mask > ALL_LEDS_ON)
+		return -EINVAL;
 
+	WRITE_ONCE(led_mask, mask);
+	mod_timer(&blink_timer, jiffies + BLINK_DELAY);
 
+	return count;
+}
+
+static struct kobj_attribute led_mask_attribute =
+	__ATTR(test, 0660, led_mask_show, led_mask_store);
 
 static int __init kbleds_init(void)
 {
+	struct tty_struct *tty;
+	int error;
 
-        int error = 0;
+	console_lock();
+	if (fg_console < 0 || fg_console >= MAX_NR_CONSOLES ||
+	    !vc_cons[fg_console].d) {
+		console_unlock();
+		pr_err("kbleds: foreground console is unavailable\n");
+		return -ENODEV;
+	}
 
-        pr_debug("Module initialized successfully \n");
+	tty = tty_kref_get(vc_cons[fg_console].d->port.tty);
+	console_unlock();
+	if (!tty || !tty->driver || !tty->driver->ops ||
+	    !tty->driver->ops->ioctl) {
+		pr_err("kbleds: foreground console has no usable TTY driver\n");
+		tty_kref_put(tty);
+		return -ENODEV;
+	}
 
-        example_kobject = kobject_create_and_add(dirname,
-                                                 kernel_kobj);
-        if(!example_kobject)
-                return -ENOMEM;
+	console_tty = tty;
+	timer_setup(&blink_timer, blink_timer_func, 0);
 
-        error = sysfs_create_file(example_kobject, &foo_attribute.attr);
-        if (error) {
-                pr_debug("failed to create the foo file in /sys/kernel/systest \n");
-        }
+	leds_kobject = kobject_create_and_add("systest", kernel_kobj);
+	if (!leds_kobject) {
+		tty_kref_put(console_tty);
+		return -ENOMEM;
+	}
 
-        int i;
-        printk(KERN_INFO "kbleds: loading\n");
-        printk(KERN_INFO "kbleds: fgconsole is %x\n", fg_console);
-        for (i = 0; i < MAX_NR_CONSOLES; i++) {
-                if (!vc_cons[i].d)
-                        break;
-                printk(KERN_INFO "poet_atkm: console[%i/%i] #%i, tty %lx\n", i,
-                       MAX_NR_CONSOLES, vc_cons[i].d->vc_num,
-                       (unsigned long)vc_cons[i].d->port.tty);
-        }
-        printk(KERN_INFO "kbleds: finished scanning consoles\n");
-        my_driver = vc_cons[fg_console].d->port.tty->driver;
-        printk(KERN_INFO "kbleds: tty driver magic %x\n", my_driver->major);
-        /*
-         * Set up the LED blink timer the first time
-         */
+	error = sysfs_create_file(leds_kobject, &led_mask_attribute.attr);
+	if (error) {
+		pr_err("kbleds: failed to create /sys/kernel/systest/test: %d\n",
+		       error);
+		kobject_put(leds_kobject);
+		tty_kref_put(console_tty);
+		return error;
+	}
 
-        return 0;
+	pr_info("kbleds: loaded; write an LED mask from 0 to 7 to "
+		"/sys/kernel/systest/test\n");
+	return 0;
 }
-static void __exit kbleds_cleanup(void)
+
+static void __exit kbleds_exit(void)
 {
-        kobject_put(example_kobject);
-        printk(KERN_INFO "kbleds: unloading...\n");
-        timer_delete(&my_timer);
-        (my_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED,
-                            RESTORE_LEDS);
-        kobject_put(example_kobject);
+	/* Stop new sysfs writes before shutting down the self-rearming timer. */
+	kobject_put(leds_kobject);
+	timer_shutdown_sync(&blink_timer);
+	set_keyboard_leds(RESTORE_LEDS);
+	tty_kref_put(console_tty);
+	pr_info("kbleds: unloaded\n");
 }
+
 module_init(kbleds_init);
-module_exit(kbleds_cleanup);
+module_exit(kbleds_exit);
